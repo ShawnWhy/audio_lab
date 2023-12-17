cf1ef3db9cb957f8634264c81c1bc6d15c5c8e8f
shawn Yu <shawnyudesign@gmail.com>
2023-12-17 11:34:28 -0500

    sdsdsd

--- local/script3.js
+++ local/script3.js
@@ -29,11 +29,49 @@ const button2 = document.getElementById("button2");
 // const visualContext = canvas.getContext('2d');
 
 // let analyzer;
+function shootcake(){
+  
+}
+function animate() {
+  x=0
+  // console.log("animate");
+  // visualContext.clearRect(0, 0, canvas.width, canvas.height);
+  analyzer.getByteFrequencyData(dataArray);
+  for (let i = 0; i < bufferLength; i++) {
+    if(dataArray[i]>80){
+    barHeight = dataArray[i];
+    
+    visualContext.fillStyle = "black";
+    visualContext.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
+    }
+    x += barWidth;
+  
+  }
+  requestAnimationFrame(animate);
+}
 
 container.addEventListener("click", ()=>{
 
 console.log("play")
   audio1.play();
+
+  audioSource = context.createMediaElementSource(audio1);
+  audioSource.connect(context.destination)
+  console.log(audioSource)
+  analyzer = context.createAnalyser();
+  audioSource.connect(analyzer);
+  analyzer.connect(context.destination);
+  analyzer.fftSize = 64;
+  console.log(analyzer)
+  console.log(audioSource)
+  const bufferLength = analyzer.frequencyBinCount;
+  const dataArray = new Uint8Array(bufferLength);
+
+  const barWidth = canvas.width / bufferLength;
+  let barHeight;
+  let x = 0;
+  animate();
+
 })
 
 
